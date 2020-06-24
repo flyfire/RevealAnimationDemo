eed57f616722b12d6b1c546d948eb9bc13ade9b5
Solarex <rh.hou.work@gmail.com>
2020-06-24 11:33:36 +0800

    percent

---
+++ app/src/main/java/com/solarexsoft/revealanimationdemo/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.solarexsoft.revealanimationdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by houruhou on 2020/6/23/2:27 PM
+ * Desc:
+ */
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
